5d95038762f2309401998bf4054a104e6ab1bada
FilipDavid1 <filipenkodavid@gmail.com>
2022-11-27 22:03:11 +0100

    add functionality to create event and push it to firestore

--- javascript/firestore.js
+++ javascript/firestore.js
@@ -2,28 +2,56 @@ import { db, collection, getDocs, addDoc, onSnapshot, deleteDoc, doc } from './f
 
 //collection reference
 const colRef = collection(db, 'services');
-
+const eventRef = collection(db, 'events');
 //real time collection data
+
+let services = [];
 onSnapshot(colRef, (querySnapshot) => {
-    let services = [];
+    
     let select = document.getElementById('select');
     let price = document.getElementById('price');
     let img = document.getElementById('img');
     let name = document.getElementById('service-name');
+    let button = document.getElementById('submit');
         querySnapshot.docs.forEach((doc) =>{
             services.push({ ...doc.data() })
         })
+        price.innerHTML = 'Cena: ' + services[0].price + '€';
+        
+        img.src = services[0].img;
+        name.innerHTML = services[0].name;
         select.innerHTML = services.map(service => `<option value="${service.name}">${service.name}</option>`).join('');
 
         select.addEventListener('change', (e) => {
             let selected = services.find(service => service.name === e.target.value);
-            if(selected.price === undefined){
-                price.innerHTML = '';
-            }
-            price.innerHTML = selected.price + '€';
+            
+            price.innerHTML = 'Cena: ' + selected.price + '€';
             img.src = selected.img;
             name.innerHTML = selected.name;
         })
+});
+
+const button = document.getElementById('submit');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let selected = services.find(service => service.name === select.value);
+    let name = document.getElementById('name').value;
+    let phone = document.getElementById('phone').value;
+    let date = document.getElementById('cal').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    let data = {
+        name,
+        phone,
+        date,
+        email,
+        message,
+        service: selected.name,
+    }
+    addDoc(eventRef, data);
+
+    
 });
 
 
