b446fce46e5f21c8edae142de4a1eb213afff979
FilipDavid1 <filipenkodavid@gmail.com>
2022-11-30 18:25:20 +0100

    load contact data from firestore

--- javascript/contact.js
+++ javascript/contact.js
@@ -10,6 +10,22 @@ const form = document.getElementById('contact-form');
 
 //collection reference
 const colRef = collection(db, 'contact');
+const contactInfoRef = collection(db, 'contact-info');
+
+//get contact info
+onSnapshot(contactInfoRef, () => {
+    let email = document.getElementById('email');
+    let phone = document.getElementById('phone');
+    let address = document.getElementById('address');
+
+    getDocs(contactInfoRef).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            email.innerHTML = doc.data().email;
+            phone.innerHTML = doc.data().phone;
+            address.innerHTML = doc.data().address;
+        });
+    });
+})
 
 //contact form
 form.addEventListener('submit', (e) => {
